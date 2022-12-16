ece912197e47ce5c89a2df843e60e168647073ca
JijoJames04 <110347193+JijoJames04@users.noreply.github.com>
2022-12-16 18:14:28 +0530

    Store number in a variable

--- home.js
+++ home.js
@@ -11,4 +11,6 @@ console.log(b);
 var someNumber = 45;
 console.log(someNumber);
 
-document.getElementById('someText').innerHTML = 'Hey There';
+var age = prompt('What is your age?');
+
+document.getElementById('someText').innerHTML = age;
