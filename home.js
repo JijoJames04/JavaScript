ef1a596caf727c4a8de4f0a7192526400b3d6fb6
JijoJames04 <110347193+JijoJames04@users.noreply.github.com>
2022-12-16 18:24:42 +0530

    variables used

--- home.js
+++ home.js
@@ -9,8 +9,29 @@ var b = 'smoothie';
 console.log(b);
 
 var someNumber = 45;
-console.log(someNumber);
+// console.log(someNumber);
 
-var age = prompt('What is your age?');
+// Manipulate DOM with Javascript
+/* ... It's just a Fancy way of saying
+change HTML with Javascript*/
 
-document.getElementById('someText').innerHTML = age;
+// var age = prompt('What is your age?');
+
+// document.getElementById('someText').innerHTML = age;
+
+// Numbers is Javascript
+var num1 = 10;
+
+// Increment num1 by 1
+num1++;
+
+// Decrement num1 by 1
+num1--;
+console.log(num1);
+
+// Divide, multiply *,remainder %
+console.log(num1 % 6);
+
+// Increment/decrement by 10
+num1 += 10;
+console.log(num1);
