3110491b4df7dd2356a3c95d18771900ea359c5b
JijoJames04 <110347193+JijoJames04@users.noreply.github.com>
2022-12-19 19:27:02 +0530

    Code for giving tip for waiter

---
+++ tip.js
@@ -0,0 +1,25 @@
+console.log('Hello World')
+console.log('Jijo James')
+
+// variables
+name = 'Peter Pan'
+console.log(name)
+
+sentence = 'how are you doing today'
+
+// operators
+// fruit = prompt('What is your favorite fruit?')
+
+//console.log(fruit)
+
+
+food = Number(prompt('how much was the food?'))
+tipPercentage = Number(prompt('tip %?')) / 100
+tipAmount = food * tipPercentage
+total = food + tipAmount
+
+console.log('tip amount',tipAmount)
+console.log('total',total)
+// alert(tipAmount)
+
+// user input
